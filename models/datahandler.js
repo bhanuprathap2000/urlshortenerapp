12e6981312d19bb53718d033213564b228812e50
Bhanu prathap <60665107+bhanuprathap2000@users.noreply.github.com>
2021-01-21 22:23:05 +0530

    Update datahandler.js

--- models/datahandler.js
+++ models/datahandler.js
@@ -2,7 +2,6 @@ const mongoose =require('mongoose');
 
 // mongoose.connect("mongodb://localhost:27017/urlshortener",{useNewUrlParser:true,useUnifiedTopology:true});
 
-mongoose.connect("mongodb+srv://admin-bhanu:123@cluster0.fhbzd.mongodb.net/urlshortener",{useNewUrlParser:true, useUnifiedTopology: true});
 const urlSchema=mongoose.Schema({
 
   longUrl:{
